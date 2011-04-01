989e4a394f381ebff63a27d5bfa0aaccf121cc57
hueicheng <pikachuliou@gmail.com>
2011-04-02 00:10:03 +0800

    index.html

--- wikiBlog/source/wikiBlog.js
+++ wikiBlog/source/wikiBlog.js
@@ -5,7 +5,6 @@ function initPage(){
 	var r=new RegExp("\\#([\\w/:\\.]+)", "gi");
 	if (location.href.match(r)) {
    	   wikiPath = RegExp.$1;
-   	   alert(wikiPath);
 	}
 	
 	getText();
@@ -14,6 +13,5 @@ function initPage(){
 function getText(){
 	
 	var url = "page/" + wikiPath.replace(/:/gi, '_')+".txt";
-	alert(url);
-	$('textPane').load(url, convert);
+	document.getElementById("textPane").innerHTML = "";
 }
